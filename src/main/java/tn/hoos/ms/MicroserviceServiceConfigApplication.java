46c363092a3193d579596daf31a0f3b46448227b
Houssem Ben Ali <houssembenali90@gmail.com>
2019-04-02 00:41:38 +0100

    first conf

--- src/main/java/tn/hoos/ms/MicroserviceServiceConfigApplication.java
+++ src/main/java/tn/hoos/ms/MicroserviceServiceConfigApplication.java
@@ -2,10 +2,16 @@ package tn.hoos.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class MicroserviceServiceConfigApplication {
-
+	
+	//after run check URL 	http://localhost:8888/application/master
+	// 						http://localhost:8888/company-service/master
+	// http:// IP:port / configFileName / profile
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceServiceConfigApplication.class, args);
 	}
