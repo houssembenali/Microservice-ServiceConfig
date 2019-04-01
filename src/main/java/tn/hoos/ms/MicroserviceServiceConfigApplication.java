70fb9c2e9a569f77df53e20a3de321a2ee876a59
Houssem Ben Ali <houssembenali90@gmail.com>
2019-04-02 00:12:23 +0100

    first commit

---
+++ src/main/java/tn/hoos/ms/MicroserviceServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package tn.hoos.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceServiceConfigApplication.class, args);
+	}
+
+}
